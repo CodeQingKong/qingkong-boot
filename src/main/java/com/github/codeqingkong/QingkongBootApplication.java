66354396ab367272bb38f39857cba5e46c81e5e9
luozhiwei <logan.zw.luo@cn.ey.com>
2023-04-04 17:54:05 +0800

    初始化项目

---
+++ src/main/java/com/github/codeqingkong/QingkongBootApplication.java
@@ -0,0 +1,16 @@
+package com.github.codeqingkong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author QingKong
+ */
+@SpringBootApplication
+public class QingkongBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QingkongBootApplication.class, args);
+    }
+
+}
